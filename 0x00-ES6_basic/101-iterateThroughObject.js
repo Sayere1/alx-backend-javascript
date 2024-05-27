577a1c0139bb340e950be5d0c0c88911590d2ed1
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-05-27 17:38:28 +0000

    Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,8 @@
 export default function createReportObject(employeesList) {
   const allEmployees = {};
   for (const [departmentName, employees] of Object.entries(employeesList)) {
-    allEmployees[departmentName] = [...employees];}
+    allEmployees[departmentName] = [...employees];
+  }
   return {
     allEmployees,
     getNumberOfDepartments() {
