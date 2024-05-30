d912a9e137c3c572e1f013239dccde3bf06bf4be
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-05-30 15:22:33 +0000

    vrooom'

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,43 @@
+/* attribute must be stored in an “underscore” attribute version. */
+/* Add a method named cloneCar, it should return a new object of the class. */
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
