24ef6c0194417f7da27ed12a97d3f4665cdc3f88
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-06-04 10:30:38 +0000

    updated

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+/**
+ * function named cleanSet that returns a string of all the
+ * set values that start with a specific string (startString).
+ * It accepts two arguments: a set (Set) and a startString (String).
+ */
+export default function cleanSet(set, startString) {
+  const setRet = [];
+   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') return '';
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      setRet.push(s.slice(startString.length));
+    }
+  });
+  return setRet.join('-');
+}
