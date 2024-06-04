0fc476da1c4816fdcbf372dbe965a03cb6d91070
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-06-04 10:04:45 +0000

    typed Array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,8 @@
+/**
+ * Creates a buffer array with a given position set to a given value.
+ * @param {Number} lenght, position, value.
+ * @returns {DataView}
+ */
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer, 0, length);
