99cb25f831700979cf6e6c41b8fc9e4f5aefd1fe
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-06-04 09:35:39 +0000

    filter func on Array

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+/**
+ * @param {{ id: Number, firstName: String, location: String }[]}.
+ * @param {String} city - The location.
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
